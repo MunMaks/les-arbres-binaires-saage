39c76de7fe3407aa37c4f79d9df7958805ce2b7c
Maks <98954111+MunMaks@users.noreply.github.com>
2024-02-16 00:52:40 +0100

    Update arbres_binaires.c

--- src/arbres_binaires.c
+++ src/arbres_binaires.c
@@ -1,1 +1,58 @@
 /* Arbre binaires */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+
+typedef struct _noeud{
+    char *val;  // discutable
+    struct _noeud *left, *right;
+} Noeud, *Arbre;
+
+
+
+Noeud *alloue_noeud(char *s)
+{
+    Noeud *noued = malloc(sizeof *noued);
+    if (!noeud) { return NULL; }
+
+    strcpy(noeud->val, s);
+    noeud->right = noeud->left = NULL;
+    return noued;
+}
+
+
+void liberer_arbre(Arbre *arbre)
+{
+    if (*arbre){
+        liberer_arbre(&(*arbre)->left);
+        liberer_arbre(&(*arbre)->right);
+        free(*arbre);
+        *arbre = NULL;
+    }
+}
+
+
+/* TO DO */
+Arbre cree_A_1(void)
+{
+
+}
+
+
+/* TO DO */
+Arbre cree_A_2(void)
+{
+  
+}
+
+
+/* TO DO */
+Arbre cree_A_3(void)
+{
+  
+}
+
+
