ad2e22e2234022502749d276a5eaadc084448602
Maks <98954111+MunMaks@users.noreply.github.com>
2024-02-16 01:56:42 +0100

    Update greffe.c

--- src/greffe.c
+++ src/greffe.c
@@ -1,1 +1,23 @@
 /* Greffe */
+
+int copie(Arbre *dest, Arbre source)
+{
+    if (!source) { *dest = NULL; return 0; }  /* Source est vide */
+
+    *dest = malloc(sizeof *source);
+
+    if (*dest) { return 0; }  /* allocation mal passe */
+      
+    if (source->left) {
+        *(dest)->left = source->left;
+        return copie(&(*dest)->left, source->left); 
+    }
+
+    if (source->right) {
+        *(dest)->right = source->right;
+        return copie(&(*dest)->right, source->right); 
+    }
+
+    return 1;
+}
+
