55d3220499d695622ca28b273e9c4d311d6bb9bc
Maks <98954111+MunMaks@users.noreply.github.com>
2024-03-10 19:27:04 +0100

    little changes in main.c

--- src/main.c
+++ src/main.c
@@ -1,39 +1,6 @@
 #include "../include/option.h"
 
 
-/*
-Soit i = {1, 2, 3}, X = {B, C, D}
-Usage d'option -G avec ou sans "exemples/"
-valgrind ./algo -G A_i.saage X.saage
-valgrind ./algo -G exemples/A_i.saage exemples/X.saage
-*/
-
-
-/*
-Soit i = {1, 2, 3}
-Usage d'option -E avec ou sans "exemples/"
-
-l'entree stadart de l'utilisateur d'apres exemples/usr_A_i.txt
-valgrind ./algo -E exemples/new_fichier.saage < exemples/usr_A_i.txt
-
-l'entree standart de l'utilisateur d'apres clavier:
-valgrind ./algo -E new_fichier.saage
-*/
-
-
-/*
-Usage d'option -DOT
-valgrind ./algo -DOT fichier.saage
-valgrind ./algo -DOT exemples/fichier.saage
-*/
-
-
-/*
-Usage d'option BIG avec ou sans "exemples/"
-valgrind ./algo -BIG exemples/grand.saage
-valgrind ./algo -BIG immense.saage 
-*/
-
 int main(int argc, char *argv[])
 {
     int i = 0;
@@ -43,9 +10,9 @@ int main(int argc, char *argv[])
         return EXIT_SUCCESS;
     }
     for (i = 1; i < argc; ++i) {
-        if ( comparer_chaines(*(argv + i), "-G") ) {
+        if ( comparer_chaines( *(argv + i), "-G") ) {
             if (i + 2 < argc) 
-                option_G_main(*(argv + 1 + i), *(argv + 2 + i));
+                option_G_main( *(argv + 1 + i), *(argv + 2 + i) );
             return EXIT_SUCCESS;
         }
 
